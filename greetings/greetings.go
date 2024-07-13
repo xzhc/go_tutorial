63e82dfa9b1bf5e7a30dd2dc308062c9f584a839
xzhc <1838998336@qq.com>
2024-07-13 14:49:30 +0800

    Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-//Hello returns a greeting for the named person
-func Hello(name string) string {
+// Hello returns a greeting for the named person
+func Hello(name string) (string, error) {
+	//If not name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	//Return a greeting tha embeds the name in a message
 	message := fmt.Sprintf("Hi, %v. welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"fmt"
-
 	"example.com/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("XZH")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	//Requesting a greeting message
+	message, err := greetings.Hello("")
+	//If an error was returned, print it to the console and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
