a686c99a9d1ded0dc4dfadac7190e52495e561a2
xzhc <1838998336@qq.com>
2024-07-14 13:40:30 +0800

    Writing Web Applications:Handling non-existent pages&Saving Pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -13,6 +13,7 @@ type Page struct {
 }
 
 func main() {
+	http.HandleFunc("/save/", saveHandler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -42,7 +43,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 // viewHandler allow users to view a wiki page
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+	}
 	renderTemplate(w, "view", p)
 }
 
@@ -55,4 +59,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
+}
+
+// saveHandler will handle the submission of forms located on the edit pages.
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
