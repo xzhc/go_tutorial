a53c5fc99d93960397d40cc9e4eb3e2698ed4674
xzhc <1838998336@qq.com>
2024-07-17 22:14:25 +0800

    V0.2_广播上线功能

--- 8HoursToBecomeAGolangEngineer/GolangStudy/golang-IM-System/server.go
+++ 8HoursToBecomeAGolangEngineer/GolangStudy/golang-IM-System/server.go
@@ -3,23 +3,66 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 // 定义server结构体
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 // 创建一个server的接口
 func NewServer(ip string, port int) *Server {
-	server := &Server{Ip: ip, Port: port}
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
 	return server
 }
 
 func (this *Server) Handler(conn net.Conn) {
 	//...当前链接的业务
-	fmt.Println("链接建立成功")
+	//fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	//用户上线后加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+}
+
+// 广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+// 监听Message广播消息的goroutine,一旦有消息就发送给全部在线的user
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将msg发送给全部在线的User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
 }
 
 // 启动服务器的接口
@@ -31,6 +74,9 @@ func (this *Server) Start() {
 	}
 	//close socket
 	defer listener.Close()
+
+	//启动监听Messenager的goroutine
+	go this.ListenMessager()
 
 	for {
 		//accept
