79cb875863e98a5561e3bf53fe70c989e6074f12
xzhc <1838998336@qq.com>
2024-07-19 10:53:29 +0800

    v0.7_超时强行下线用户

--- 8HoursToBecomeAGolangEngineer/GolangStudy/golang-IM-System/server.go
+++ 8HoursToBecomeAGolangEngineer/GolangStudy/golang-IM-System/server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 // 定义server结构体
@@ -36,6 +37,10 @@ func (this *Server) Handler(conn net.Conn) {
 	//fmt.Println("链接建立成功")
 
 	user := NewUser(conn, this)
+	user.Online()
+
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
 
 	//接受客户端发送的消息
 	go func() {
@@ -55,11 +60,26 @@ func (this *Server) Handler(conn net.Conn) {
 			msg := string(buf[:n-1])
 			//得到的消息进行广播
 			user.DoMessage(msg)
-
+			//用户的任意消息代表当前用户是活跃的
+			isLive <- true
 		}
 	}()
 	//当前handler堵塞
-	select {}
+	select {
+	case <-isLive:
+	//当前用户是活跃的，应该重置定时器
+	//不做任何事情，为了激活select，更新下面的定时器
+
+	case <-time.After(time.Second * 10):
+		//超时将当前user强制关闭
+		user.sendMsg("you are offline because time exceed!")
+		//销毁用的资源
+		close(user.C)
+		//关闭链接
+		conn.Close()
+		//退出当前handler
+		return
+	}
 }
 
 // 广播消息的方法
